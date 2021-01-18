a25634954451079634912dd006986aca3d304759
Motomi Ikeda <ikdmtm01@gmail.com>
2021-01-18 21:11:44 +0900

    add gpio program

--- myled.c
+++ myled.c
@@ -17,7 +17,17 @@ static volatile u32 *gpio_base = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-	printk(KEN_INFO "led_write is called\n");
+	char c;
+	if(copy_from_user(&c, buf, sizeof(char)))
+		return -EFAULT;
+
+	//printk(KEN_INFO "led_write is called\n");
+	
+	if(c == '0')
+		gpio_base[10] = 1 << 25;
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
+
 	return 1;
 }
 
