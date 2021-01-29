b14bf829a6b2bb7758f191143404c63168525f22
ikdmtm <ikdmtm01@gmail.com>
2021-01-29 15:47:23 +0000

    fix code

--- myled.c
+++ myled.c
@@ -2,8 +2,8 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
+#include <linux/uaccess.h>
 #include <linux/io.h>
-
 
 MODULE_AUTHOR("Motomi Ikeda");
 MODULE_DESCRIPTION("driver for LED control");
@@ -18,21 +18,15 @@ static volatile u32 *gpio_base = NULL;
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
 	char c;
-	int n=0, second=1;
-	if(copy_from_user(&c, buf, sizeof(char)))
+	if(copy_from_user(&c,buf,sizeof(char)))
 		return -EFAULT;
 
-	//printk(KEN_INFO "led_write is called\n");
-		
-	
-	
-	for(n=0; c+12>n; n++){
-		if(c == '0')
-			gpio_base[10] = 1 << 25;
-		else
-			gpio_base[7] = 1 << 25;
-		sleep(second);
-	}
+	//printk(KERN_INFO "receive %c\n");
+	if(c == '0')
+		gpio_base[10] = 1 << 25;
+	else if(c == '1')
+		gpio_base[7] = 1 << 25;
+
 	return 1;
 }
 
@@ -41,40 +35,41 @@ static struct file_operations led_fops = {
 	.write = led_write
 };
 
-static int __init init_mod(void){
+static int __init init_mod(void)
+{
 	int retval;
 	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
-
-	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__, MAJOR(dev));
-	cdev_init(&cdv, dev, 1);
-	retval = cdev_add(&cdv, dev, 1);
-	if(retval < 0){
-		printk(KERN_ERR "cdev_add failed. major:%d. minor:%d", MAJOR(dev));
-		return retval;
-	}
-
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev_region failed.\n");
 		return retval;
 	}
-	printk(KERN_INFO "%s is loaded.\n",__FILE__);
 
-	cls = class_create(THIS_MODULE."myled");
+	printk(KERN_INFO "%s is loaded. major:%d\n",__FILE__,  MAJOR(dev));
+	
+	cdev_init(&cdv, &led_fops);
+	retval = cdev_add(&cdv,  dev, 1);
+	if(retval < 0){
+		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d", MAJOR(dev), MINOR(dev));
+		return retval;
+	}
+	
+	cls = class_create(THIS_MODULE, "myled");
 	if(IS_ERR(cls)){
-		printk(KERN_ERR "class_create failed".);
+		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
-	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
-	gpio_base = ioremap_nocashe(0x3f200000, 0xA0);
+	}
+	device_create(cls,  NULL, dev,  NULL, "myled%d",MINOR(dev));
 
+	gpio_base = ioremap_nocache(0x3f200000,  0xA0);
+	
 	const u32 led = 25;
 	const u32 index = led/10;
 	const u32 shift = (led%10)*3;
-	const u32 mask = (0x7 << shift);
+	const u32 mask = ~(0x7 << shift);
 	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
 
 	return 0;
 }
-
 
 static void __exit cleanup_mod(void)
 {
@@ -82,9 +77,8 @@ static void __exit cleanup_mod(void)
 	device_destroy(cls, dev);
 	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
-	printk(KERN_INFO "%S is unloaded. major:%d\n",__FILE__,MAJOR(dev));
+	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__, MAJOR(dev));
 }
 
 module_init(init_mod);
 module_exit(cleanup_mod);
-
